4c6d4f39347b61d1d6d7ea1decc1122cc536dc71
Edgar Velazquez <edantonio505@gmail.com>
2016-07-06 07:49:01 -0400

    Asdf

--- resources/views/partials/dashboard_bar.blade.php
+++ resources/views/partials/dashboard_bar.blade.php
@@ -1,4 +1,4 @@
-<div style="background-color:#12d4b4; min-height: 100px; width: 100%; margin-top: 47px;">
+<div style="background-color:#e8e8e8; min-height: 100px; width: 100%; margin-top: 47px;">
 	<div class="row">
 		<div class="large-10 large-centered columns">
 			<div class="row">
